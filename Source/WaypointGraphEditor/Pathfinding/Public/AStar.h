674d059e48a5e1297983a6785acd3c64d51b54a0
kraml <kramlichtyler@gmail.com>
2023-03-10 00:07:23 -0500

    Restructure

---
+++ Source/WaypointGraphEditor/Pathfinding/Public/AStar.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AStar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAYPOINTGRAPHEDITOR_API UAStar : public UObject
+{
+	GENERATED_BODY()
+};
